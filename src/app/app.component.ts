7eca74a4425db299b9c59865563ddca46069eff2
Alexis Gonzalez <1991.alexis@gmail.com>
2023-04-27 19:10:58 -0400

    hice funcionar el betulio del PDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgxImageCompressService } from 'ngx-image-compress';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,29 @@ import { NgxImageCompressService } from 'ngx-image-compress';
 })
 export class AppComponent {
   title = 'image-compression-testing';
-  image: any = undefined;
-  constructor(private imageCompress: NgxImageCompressService) {}
+  pdfUrl: any;
+  imgUrl: any;
+
+  constructor(
+    private imageCompress: NgxImageCompressService,
+    private sanitizer: DomSanitizer
+  ) {}
+
   async onFileSelected(event: any) {
-    console.log(event.target.files[0]);
-    this.image = await this.fileToDataURL(event.target.files[0]);
-    console.log(this.image);
-    // this.imageCompress.compressFile()
+    let base64pdf = await this.fileToDataURL(event.target.files[0]);
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(base64pdf);
   }
+
+  async onImageSelected(event: any) {
+    let base64img = await this.fileToDataURL(event.target.files[0]);
+    console.log(base64img);
+  }
+
   fileToDataURL(file: File): Promise<string> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = () => resolve(reader.result as string);
-      reader.onerror = error => reject(error);
+      reader.onerror = (error) => reject(error);
       reader.readAsDataURL(file);
     });
   }
